a6b1772cdb07618ed49e413aa9fa6f2f3170db1d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-03-31 10:39:51 -0400

    trying to figure out how much overhead the format is adding to the data representation

--- example_usage/example.py
+++ example_usage/example.py
@@ -14,20 +14,29 @@ def randomize_wx():
     # want to make 'realistic' weather
     random.seed(0)
     for feature in condensed['features']:
-        for source, members in feature['properties']['data'].items():
+        for source in feature['properties']['data'].keys():
+            members = condensed['data'][source].get('members', [0])
+            member = feature['properties']['data'][source][0]
             n = len(condensed['data'][source]['time']['values'])
-            feature['properties']['data'][source] = [[generate(list(condensed['indices'].keys())[i], n) if isinstance(member[i], list) else None for i in range(len(member))] for member in members]
+            def gen_member(member):
+                return [generate(list(condensed['indices'].keys())[i], n) if isinstance(member[i], list) else None for i in range(len(member))]
+            feature['properties']['data'][source] = [gen_member(member) for i in range(len(members))]
     # make sure it's the same for both file formats
     random.seed(0)
     for feature in uncommented['features']:
-        for source, members in feature['properties']['data'].items():
+        for source in feature['properties']['data'].keys():
             n = len(uncommented['data'][source]['time']['values'])
+            members = condensed['data'][source].get('members', None)
             def gen_members(member):
                 return {k: generate(k, n) for k in member.keys()}
-            if dict == type(feature['properties']['data'][source]):
-                feature['properties']['data'][source] = {k: gen_members(v) for k, v in members.items()}
+            if members is None:
+                member = feature['properties']['data'][source][0]
+                feature['properties']['data'][source] = [gen_members(member)]
             else:
-                feature['properties']['data'][source] = [gen_members(v) for v in members]
+                # use first item as template for keys to use for members
+                member = list(feature['properties']['data'][source].values())[0]
+                feature['properties']['data'][source] = {k: gen_members(member) for k in members}
+
 
 
 randomize_wx()
@@ -58,3 +67,53 @@ assert x == y
 
 save(condensed, FILE_CONDENSED)
 save(uncommented, FILE_UNCOMMENTED)
+
+
+def dumps_min(data):
+    import json
+    return json.dumps(data, separators=(',', ':'))
+
+
+def find_size(data):
+    return len(dumps_min(data))
+
+
+# make an imaginary reference minimum size based on somehow being able
+# to just put data in the right order without any keys
+def make_min(data):
+    if isinstance(data, list):
+        return '[' + ','.join([make_min(v) for v in data]) + ']'
+    elif isinstance(data, dict):
+        # just look at size of data and not keys
+        return make_min([v for k, v in data.items() if k != 'type'])
+    return dumps_min(data)
+
+
+
+
+def overhead(data):
+    # use condensed since it has null values for things where uncommented has no keys
+    min_size = len(make_min(condensed))
+    return (find_size(data) - min_size) / min_size
+
+
+def summarize(name, data):
+    return(f'Overhead for {name} format is {(overhead(data) * 100):0.2f}%')
+
+print(summarize("condensed", condensed))
+print(summarize("uncommented", uncommented))
+
+
+# try to figure out how much overhead there would be with an actual set of data
+MEMBERS = [str(x) for x in range(40)] + ["control"]
+
+condensed['data']['iefs']['members'] = MEMBERS
+uncommented['data']['iefs']['members'] = MEMBERS
+
+randomize_wx()
+
+save(condensed, FILE_CONDENSED.replace('.geojson', '_complete.geojson'))
+save(uncommented, FILE_UNCOMMENTED.replace('.geojson', '_complete.geojson'))
+
+print(summarize("condensed complete", condensed))
+print(summarize("uncommented complete", uncommented))
